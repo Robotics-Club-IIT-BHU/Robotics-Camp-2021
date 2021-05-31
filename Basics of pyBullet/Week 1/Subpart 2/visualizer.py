83d0eabbf3a69b5baaab7f19e290f1497b26fb0c
SHIVANSH DUBEY <dshivansh92@gmail.com>
2021-05-31 23:16:05 +0530

    2

--- Basics of pyBullet/Week 1/Subpart 2/visualizer.py
+++ Basics of pyBullet/Week 1/Subpart 2/visualizer.py
@@ -7,7 +7,7 @@ urdf file in the same folder as that of the python script
 '''
 file_path = os.getcwd()
 
-#Enter the file name with a '/' in front of it 
+# Enter the file name with a '/' in front of it
 file_name = "/sample.urdf"
 '''
 these comands are explained in detail in the next subpart
@@ -17,7 +17,7 @@ p.connect(p.GUI)
 p.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"), 0, 0, 0)
 robot = p.loadURDF(file_path+file_name)
 p.resetBasePositionAndOrientation(robot, [0, 0, 1], [0, 0, 0, 0.707])
-p.setGravity(0,0,0)
+p.setGravity(0, 0, -1)
 
 while(True):
-	p.stepSimulation()
+    p.stepSimulation()
