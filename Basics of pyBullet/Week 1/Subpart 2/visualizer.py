d98ab1d9096838138b850da0d6c753217a46d851
SHIVANSH DUBEY <dshivansh92@gmail.com>
2021-06-02 10:18:05 +0530

    2jun

--- Basics of pyBullet/Week 1/Subpart 2/visualizer.py
+++ Basics of pyBullet/Week 1/Subpart 2/visualizer.py
@@ -2,22 +2,27 @@
 import pybullet as p
 import pybullet_data
 import os
+import time
 '''
 urdf file in the same folder as that of the python script
 '''
-file_path = os.getcwd()
+# file_path = os.getcwd()
 
 # Enter the file name with a '/' in front of it
-file_name = "/sample.urdf"
+file_name = "/robourdfcode.urdf"
 '''
 these comands are explained in detail in the next subpart
 for now u can directly use it to visualize the model
 '''
 p.connect(p.GUI)
 p.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"), 0, 0, 0)
-robot = p.loadURDF(file_path+file_name)
-p.resetBasePositionAndOrientation(robot, [0, 0, 1], [0, 0, 0, 0.707])
+robot = p.loadURDF(
+    "robourdfcode.urdf")
+p.resetBasePositionAndOrientation(robot, [0, 0, 0], [0, 0, 0, 0.707])
 p.setGravity(0, 0, 0)
 
 while(True):
     p.stepSimulation()
+time.sleep(2)
+
+# print(os.getcwd())
